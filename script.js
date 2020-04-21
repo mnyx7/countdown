8cc6b3930d427e5d687f284fb149368a1cf54bc0
Minaya Yagubova <61638622+mnyx7@users.noreply.github.com>
2020-04-21 15:09:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let years = document.getElementById("years");
-let monthes = document.getElementById("monthes");
-let days = document.getElementById("days");
 let monthArr = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "ДеDecemberкабрь"];
-let daysArr = [[2], [4, 6, 9, 11], [1, 3, 5, 7, 8, 10, 12]];
 for (i = 1; i < monthArr.length; i++) {
     monthes.innerHTML += "<option>" + monthArr[i] + "</option>";
 }
 
+function countDown() {
+    let years = document.getElementById("years");
+    let days = document.getElementById("days");
+    let monthes = document.getElementById("monthes");
+
     let newData = years.value + monthes.value + days.value;
-    let event = document.getElementById('event').innerHTML;
+    let event = document.getElementById('event').value;
 
     let countDownDate = new Date(newData).getTime();
-function countDown() {
+
+
     let x = setInterval(function () {
-
         let now = new Date().getTime();
-
         let distance = countDownDate - now;
 
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let days = Math.floor(parseInt(distance) / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((parseInt(distance) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((parseInt(distance) % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((parseInt(distance) % (1000 * 60)) / 1000);
 
-        document.getElementById("userEvent").innerHTML = "Till " + event +"left " + days + "d " + hours + "h "
+        document.getElementById("userEvent").innerHTML = "Till " + event + " left " + days + "d " + hours + "h "
             + minutes + "m " + seconds + "s ";
 
         if (distance < 0) {
@@ -31,4 +31,5 @@ function countDown() {
             document.getElementById("userEvent").innerHTML = "Expired";
         }
     }, 1000);
+
 }
